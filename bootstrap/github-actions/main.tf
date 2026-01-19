9c6896bfeb61c725ea96ba5294ac64b0a197f5ea
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-19 10:53:30 +0000

    file update

--- bootstrap/github-actions/main.tf
+++ bootstrap/github-actions/main.tf
@@ -11,21 +11,42 @@ resource "aws_iam_role" "github_actions" {
   name = "github-actions-terraform-role-1"
 
   assume_role_policy = jsonencode({
-  Version = "2012-10-17"
-  Statement = [{
-    Effect = "Allow"
-    Principal = {
-      Federated = data.aws_iam_openid_connect_provider.github.arn
-    }
-    Action = "sts:AssumeRoleWithWebIdentity"
-    Condition = {
-      StringLike = {
-        "token.actions.githubusercontent.com:sub" = "repo:Kalyani-Bambal/terraform-infra:*"
+    Version = "2012-10-17"
+    Statement = [
+
+      # ---------------------------------
+      # 1️⃣ GitHub Actions (OIDC trust)
+      # ---------------------------------
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:Kalyani-Bambal/terraform-infra:*"
+          }
+        }
+      },
+
+      # ---------------------------------
+      # 2️⃣ IAM User + Role trust (YOUR JSON)
+      # ---------------------------------
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::358871393576:user/Kalyani-Bambal",
+            "arn:aws:iam::358871393576:role/github-actions-terraform-role-1"
+          ]
+        }
+        Action = "sts:AssumeRole"
       }
-    }
-  }]
-})
+    ]
+  })
 }
+
 
 resource "aws_iam_role_policy_attachment" "admin" {
   role       = aws_iam_role.github_actions.name
