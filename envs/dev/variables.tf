57bfed02c2fed87fe8d30404e9be4fd183fe28c2
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-19 12:58:31 +0530

    update the file (#23)
    * update the file
    
    * update the file

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,111 +1,111 @@
-# variable "cidr_block" {
-#   description = "The CIDR block for the VPC"
-#   type        = string
-# }
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
 
-# variable "env" {
-#   description = "Environment name"
-#   type        = string
-# }
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
 
-# variable "tags" {
-#   description = "Tags to apply to the VPC"
-#   type        = map(string)
-# }
+variable "tags" {
+  description = "Tags to apply to the VPC"
+  type        = map(string)
+}
 
 variable "region" {
   type = string
 }
 
-# variable "public_subnets" {
-#   description = "Public Subnets"
-#   type        = list(string)
-# }
+variable "public_subnets" {
+  description = "Public Subnets"
+  type        = list(string)
+}
 
-# variable "azs" {
-#   description = "Availabilty Zone"
-#   type        = list(string)
-# }
+variable "azs" {
+  description = "Availabilty Zone"
+  type        = list(string)
+}
 
-# variable "private_subnets" {
-#   description = "Private Subnets"
-#   type        = list(string)
-# }
+variable "private_subnets" {
+  description = "Private Subnets"
+  type        = list(string)
+}
 
-# variable "cluster_name" {
-#   type = string
-# }
+variable "cluster_name" {
+  type = string
+}
 
-# variable "cluster_version" {
-#   type = string
-# }
+variable "cluster_version" {
+  type = string
+}
 
-# variable "vpc_id" {
-#   description = "The ID of the VPC to deploy EKS into"
-#   type        = string
-#   default     = ""
-# }
+variable "vpc_id" {
+  description = "The ID of the VPC to deploy EKS into"
+  type        = string
+  default     = ""
+}
 
-# variable "private_subnet_ids" {
-#   description = "List of private subnet IDs for EKS cluster"
-#   type        = list(string)
-#   default     = []
-# }
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for EKS cluster"
+  type        = list(string)
+  default     = []
+}
 
-# variable "desired_size" {
-#   type    = number
-#   default = 2
-# }
+variable "desired_size" {
+  type    = number
+  default = 2
+}
 
-# variable "max_size" {
-#   type    = number
-#   default = 4
-# }
+variable "max_size" {
+  type    = number
+  default = 4
+}
 
-# variable "min_size" {
-#   type    = number
-#   default = 1
-# }
+variable "min_size" {
+  type    = number
+  default = 1
+}
 
-# variable "node_instance_types" {
-#   type = list(string)
-# }
+variable "node_instance_types" {
+  type = list(string)
+}
 
-# variable "allowed_cidr_blocks" {
-#   description = "CIDR blocks allowed to access EKS private endpoint"
-#   type = list(string)
-#   default = [  ]
-# }
+variable "allowed_cidr_blocks" {
+  description = "CIDR blocks allowed to access EKS private endpoint"
+  type = list(string)
+  default = [  ]
+}
 
 
-# variable "public_subnet_ids" {
-#   description = "Public Subnet ID for Bastion Host"
-#   type        =  list(string)
-#   default     = []
-# }
+variable "public_subnet_ids" {
+  description = "Public Subnet ID for Bastion Host"
+  type        =  list(string)
+  default     = []
+}
 
-# variable "bastion_ami" {
-#   description = "bastion_ami"
-#   type = string
-# }
+variable "bastion_ami" {
+  description = "bastion_ami"
+  type = string
+}
 
-# variable "bastion_key_name" {
-#   description = "Key pair name for bastion host"
-#   type = string
-# }
+variable "bastion_key_name" {
+  description = "Key pair name for bastion host"
+  type = string
+}
 
-# variable "common_tags" {
-#   description = "Common tags for resources"
-#   type        = map(string)
-#   default     = {}
-# }
+variable "common_tags" {
+  description = "Common tags for resources"
+  type        = map(string)
+  default     = {}
+}
 
-# variable "bastion_instance_type" {
-#   description = "Instance type for bastion host"
-#   type = string
+variable "bastion_instance_type" {
+  description = "Instance type for bastion host"
+  type = string
   
-# }
+}
 
-# variable "allowed_ssh_cidrs" {
-#   type = list(string)
-# }
+variable "allowed_ssh_cidrs" {
+  type = list(string)
+}
