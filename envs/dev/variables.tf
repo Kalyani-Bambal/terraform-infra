95ad5ce87f1640443f467250aa401b87462a59de
softconsist-in <softconsist@gmail.com>
2026-01-21 23:22:18 +0530

    dest (#52)
    Co-authored-by: Kalyani-Bambal <kalyanibambal05@yahoo.com>

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,117 +1,117 @@
-variable "cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
-}
+# variable "cidr_block" {
+#   description = "The CIDR block for the VPC"
+#   type        = string
+# }
 
-variable "env" {
-  description = "Environment name"
-  type        = string
-}
+# variable "env" {
+#   description = "Environment name"
+#   type        = string
+# }
 
-variable "tags" {
-  description = "Tags to apply to the VPC"
-  type        = map(string)
-}
+# variable "tags" {
+#   description = "Tags to apply to the VPC"
+#   type        = map(string)
+# }
 
 variable "region" {
   type = string
 }
 
-variable "public_subnets" {
-  description = "Public Subnets"
-  type        = list(string)
-}
+# variable "public_subnets" {
+#   description = "Public Subnets"
+#   type        = list(string)
+# }
 
-variable "azs" {
-  description = "Availabilty Zone"
-  type        = list(string)
-}
+# variable "azs" {
+#   description = "Availabilty Zone"
+#   type        = list(string)
+# }
 
-variable "private_subnets" {
-  description = "Private Subnets"
-  type        = list(string)
-}
+# variable "private_subnets" {
+#   description = "Private Subnets"
+#   type        = list(string)
+# }
 
-variable "cluster_name" {
-  type = string
-}
-
-variable "cluster_version" {
-  type = string
-}
-
-variable "vpc_id" {
-  description = "The ID of the VPC to deploy EKS into"
-  type        = string
-  default     = ""
-}
-
-variable "private_subnet_ids" {
-  description = "List of private subnet IDs for EKS cluster"
-  type        = list(string)
-  default     = []
-}
-
-variable "desired_size" {
-  type    = number
-  default = 2
-}
-
-variable "max_size" {
-  type    = number
-  default = 4
-}
-
-variable "min_size" {
-  type    = number
-  default = 1
-}
-
-variable "node_instance_types" {
-  type = list(string)
-}
-
-variable "allowed_cidr_blocks" {
-  description = "CIDR blocks allowed to access EKS private endpoint"
-  type = list(string)
-  default = [  ]
-}
-
-
-variable "public_subnet_ids" {
-  description = "Public Subnet ID for Bastion Host"
-  type        =  list(string)
-  default     = []
-}
-
-# variable "bastion_ami" {
-#   description = "bastion_ami"
+# variable "cluster_name" {
 #   type = string
 # }
 
-# variable "bastion_key_name" {
-#   description = "Key pair name for bastion host"
+# variable "cluster_version" {
 #   type = string
 # }
 
-variable "common_tags" {
-  description = "Common tags for resources"
-  type        = map(string)
-  default     = {}
-}
-
-# variable "bastion_instance_type" {
-#   description = "Instance type for bastion host"
-#   type = string
-  
+# variable "vpc_id" {
+#   description = "The ID of the VPC to deploy EKS into"
+#   type        = string
+#   default     = ""
 # }
 
-# variable "allowed_ssh_cidr" {
+# variable "private_subnet_ids" {
+#   description = "List of private subnet IDs for EKS cluster"
+#   type        = list(string)
+#   default     = []
+# }
+
+# variable "desired_size" {
+#   type    = number
+#   default = 2
+# }
+
+# variable "max_size" {
+#   type    = number
+#   default = 4
+# }
+
+# variable "min_size" {
+#   type    = number
+#   default = 1
+# }
+
+# variable "node_instance_types" {
 #   type = list(string)
 # }
 
-variable "bastion_assume_role_principals" {
-  description = "List of principals allowed to assume the bastion role"
-  type        = list(string)
-  default     = []
-}
+# variable "allowed_cidr_blocks" {
+#   description = "CIDR blocks allowed to access EKS private endpoint"
+#   type = list(string)
+#   default = [  ]
+# }
+
+
+# variable "public_subnet_ids" {
+#   description = "Public Subnet ID for Bastion Host"
+#   type        =  list(string)
+#   default     = []
+# }
+
+# # variable "bastion_ami" {
+# #   description = "bastion_ami"
+# #   type = string
+# # }
+
+# # variable "bastion_key_name" {
+# #   description = "Key pair name for bastion host"
+# #   type = string
+# # }
+
+# variable "common_tags" {
+#   description = "Common tags for resources"
+#   type        = map(string)
+#   default     = {}
+# }
+
+# # variable "bastion_instance_type" {
+# #   description = "Instance type for bastion host"
+# #   type = string
+  
+# # }
+
+# # variable "allowed_ssh_cidr" {
+# #   type = list(string)
+# # }
+
+# variable "bastion_assume_role_principals" {
+#   description = "List of principals allowed to assume the bastion role"
+#   type        = list(string)
+#   default     = []
+# }
