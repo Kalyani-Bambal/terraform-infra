8f73742dd399747a91bd089906f139ae95cb306a
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-21 17:32:07 +0530

    txt (#44)

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,48 +2,48 @@ provider "aws" {
   region = var.region
 }
 
-# module "vpc" {
-#   source = "git::https://github.com/Kalyani-Bambal/terraform-module.git//modules/vpc?ref=main"
+module "vpc" {
+  source = "git::https://github.com/Kalyani-Bambal/terraform-module.git//modules/vpc?ref=main"
 
-#   env            = var.env
-#   cidr_block     = var.cidr_block
-#   tags           = var.tags
-#   region         = var.region
-#   public_subnet  = var.public_subnets
-#   private_subnet = var.private_subnets
-#   azs            = var.azs
-# }
+  env            = var.env
+  cidr_block     = var.cidr_block
+  tags           = var.tags
+  region         = var.region
+  public_subnet  = var.public_subnets
+  private_subnet = var.private_subnets
+  azs            = var.azs
+}
 
-# module "eks" {
-#   source = "git::https://github.com/Kalyani-Bambal/terraform-module.git//modules/eks?ref=main"
+module "eks" {
+  source = "git::https://github.com/Kalyani-Bambal/terraform-module.git//modules/eks?ref=main"
 
-#   env             = var.env
-#   cluster_name    = "${var.env}-eks"
-#   cluster_version = var.cluster_version
+  env             = var.env
+  cluster_name    = "${var.env}-eks"
+  cluster_version = var.cluster_version
 
-#   vpc_id              = module.vpc.aws_vpc
-#   private_subnet_ids  = module.vpc.private_subnet_ids
-#   desired_size        = var.desired_size
-#   max_size            = var.max_size
-#   min_size            = var.min_size
-#   node_instance_types = var.node_instance_types
+  vpc_id              = module.vpc.aws_vpc
+  private_subnet_ids  = module.vpc.private_subnet_ids
+  desired_size        = var.desired_size
+  max_size            = var.max_size
+  min_size            = var.min_size
+  node_instance_types = var.node_instance_types
 
-#   allowed_cidr_blocks = var.allowed_cidr_blocks
+  allowed_cidr_blocks = var.allowed_cidr_blocks
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
 
-# module "bastion" {
-#   source = "git::https://github.com/Kalyani-Bambal/terraform-module.git//modules//bastion?ref=main"
+module "bastion" {
+  source = "git::https://github.com/Kalyani-Bambal/terraform-module.git//modules//bastion?ref=main"
 
-#   env           = var.env
-#   vpc_id                = module.vpc.aws_vpc
-#   public_subnet_ids     = module.vpc.public_subnet_ids
+  env           = var.env
+  vpc_id                = module.vpc.aws_vpc
+  public_subnet_ids     = module.vpc.public_subnet_ids
 
-#   bastion_ami           = var.bastion_ami
-#   bastion_instance_type = var.bastion_instance_type
-#   bastion_key_name      = var.bastion_key_name
-#   allowed_ssh_cidrs = var.allowed_ssh_cidrs
-#   common_tags       = var.common_tags
-# }
+  bastion_ami           = var.bastion_ami
+  bastion_instance_type = var.bastion_instance_type
+  bastion_key_name      = var.bastion_key_name
+  allowed_ssh_cidrs = var.allowed_ssh_cidrs
+  common_tags       = var.common_tags
+}
