b3143c78fe89ac96665a43cc3f2eecb70316f360
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-23 16:52:10 +0000

    destroy

--- envs/dev/output.tf
+++ envs/dev/output.tf
@@ -1,35 +1,35 @@
-output "aws_vpc" {
-  value = module.vpc.aws_vpc
-}
-
-output "region" {
-  value = var.region
-}
-
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_ca" {
-  value = module.eks.cluster_ca
-}
-
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
-
-# output "bastion_role_arn" {
-#   value = module.bastion.role_arn
+# output "aws_vpc" {
+#   value = module.vpc.aws_vpc
 # }
 
-output "bastion_access_role_arn" {
-  value = module.bastion.bastion_access_role_arn
-}
+# output "region" {
+#   value = var.region
+# }
+
+# output "private_subnet_ids" {
+#   value = module.vpc.private_subnet_ids
+# }
+
+# output "cluster_name" {
+#   value = module.eks.cluster_name
+# }
+
+# output "cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
+
+# output "cluster_ca" {
+#   value = module.eks.cluster_ca
+# }
+
+# output "public_subnet_ids" {
+#   value = module.vpc.public_subnet_ids
+# }
+
+# # output "bastion_role_arn" {
+# #   value = module.bastion.role_arn
+# # }
+
+# output "bastion_access_role_arn" {
+#   value = module.bastion.bastion_access_role_arn
+# }
