d8b5dd6df1e00a302e4c5bc8b92597f8a46eadc0
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2026-01-24 18:58:00 +0000

    destroy

--- envs/dev/output.tf
+++ envs/dev/output.tf
@@ -1,31 +1,31 @@
-output "aws_vpc" {
-  value = module.vpc.aws_vpc
-}
+# output "aws_vpc" {
+#   value = module.vpc.aws_vpc
+# }
 
-output "region" {
-  value = var.region
-}
+# output "region" {
+#   value = var.region
+# }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
+# output "private_subnet_ids" {
+#   value = module.vpc.private_subnet_ids
+# }
 
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   value = module.eks.cluster_name
+# }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
 
-output "cluster_ca" {
-  value = module.eks.cluster_ca
-}
+# output "cluster_ca" {
+#   value = module.eks.cluster_ca
+# }
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
+# output "public_subnet_ids" {
+#   value = module.vpc.public_subnet_ids
+# }
 
-output "bastion_access_role_arn" {
-  value = module.bastion.bastion_access_role_arn
-}
+# output "bastion_access_role_arn" {
+#   value = module.bastion.bastion_access_role_arn
+# }
